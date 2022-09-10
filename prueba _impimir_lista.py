39b77d4a7139d6f791892d40bafb29f6043e32c9
¨Paulo <paulohumano@gmail.com>
2022-09-10 20:13:36 -0300

    ya se importa lista

--- prueba _impimir_lista.py
+++ prueba _impimir_lista.py
@@ -1,12 +1,11 @@
-import creacion_menu
+import pickle
 
-
+archivo_menus = open('lista_menu', 'rb')
+lista_menus = pickle.load(archivo_menus)
 print(lista_menus)
+archivo_menus.close()
 
-"""for i in len(lista_menu):
-
-    print(lista_menu.index(i), end='\t')
-    print(lista_menu[i], end='\t')
-    print(lista_precio[i])
-return
-"""
+archivo_precios = open('lista_precio', 'rb')
+lista_precios = pickle.load(archivo_precios)
+print(lista_precios)
+archivo_precios.close()
